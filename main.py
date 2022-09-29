acbf842b5e43a0cdd8aa6640a23130ef1f988cba
Austin Hargis <theaustinhargis@gmail.com>
2022-09-29 15:19:22 -0400

    Updated cog loading to be dynamic, no need to update a list in 'main.py', just drop your cog in the 'cogs' folder and start the bot!

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from dotenv import load_dotenv
 import logging
 import os
 
-cogs_list = [
-    'greetings'
-]
-
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -17,9 +13,11 @@ load_dotenv()
 
 bot = discord.Bot()
 
+
 @bot.event
 async def on_member_join(member):
     await member.send(f'Welcome to the server, {member}. Please enjoy your stay!')
+
 
 @bot.event
 async def on_ready():
@@ -34,12 +32,12 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-
-@bot.slash_command()
-async def hello(ctx):
-    await ctx.respond('Hello!')
+for cog in os.listdir(f'./cogs/'):
+    if cog.endswith(f'.py'):
+        try:
+            bot.load_extension(f'cogs.{cog.replace(".py", "")}')
+            print(f'Loaded {cog}')
+        except discord.NoEntryPointError:
+            print(f'Unable to load {cog} because it has no \'setup\' function')
 
 bot.run(os.getenv('TOKEN'))
-
-for cog in cogs_list:
-    bot.load_extension(f'cogs.{cog}')
