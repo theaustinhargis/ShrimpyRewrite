9d8e1d75953ec0e78bf9da994f833b7035f02ebb
Austin Hargis <theaustinhargis@gmail.com>
2022-09-29 15:17:50 -0400

    Added the 'Developer' cog, which includes functionality to load, reload, and unload cogs without restarting the bot. It also includes a command to check latency.

---
+++ cogs/developer.py
@@ -0,0 +1,52 @@
+import os
+
+import discord
+from discord.ext import commands
+
+
+class Developer(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(name="latency", description="Returns Shrimpy's latency")
+    async def latency(self, ctx):
+        await ctx.respond(f'Current latency is {self.bot.latency:.2f}ms')
+
+    @commands.slash_command(name="reload_extension", description="Reloads specified extensions(s)")
+    async def reload_cog(self, ctx, extension: discord.Option(discord.SlashCommandOptionType.string, description="An extension for Shrimpy")):
+        if ctx.author.id == int(os.getenv('OWNER')):
+            self.bot.reload_extension(f'cogs.{extension}')
+            await ctx.respond(f'Reloaded cog.{extension}')
+        else:
+            await ctx.respond(f'You don\'t have permission to use this command.')
+
+    @commands.slash_command(name="load_ext", description="Loads specified extensions(s)")
+    async def load_cog(self, ctx, extension: discord.Option(discord.SlashCommandOptionType.string, description="An extension for Shrimpy")):
+        try:
+            if ctx.author.id == int(os.getenv('OWNER')):
+                self.bot.load_extension(f'cogs.{extension}')
+                await ctx.respond(f'Loaded cog.{extension}')
+            else:
+                await ctx.respond(f'You don\'t have permission to use this command.')
+        except discord.ExtensionAlreadyLoaded:
+            await ctx.respond(f'Cog {extension} is already loaded.')
+        except discord.ExtensionNotFound:
+            await ctx.respond(f'Cog {extension} was not found, please try again.')
+
+    @commands.slash_command(name="unload_extension", description="Unloads specified extension(s)")
+    async def unload_cog(self, ctx, extension: discord.Option(discord.SlashCommandOptionType.string, description="An extension for Shrimpy")):
+        try:
+            if ctx.author.id == int(os.getenv('OWNER')):
+                self.bot.unload_extension(f'cogs.{extension}')
+                await ctx.respond(f'Unloaded cog.{extension}')
+            else:
+                await ctx.respond(f'You don\'t have permission to use this command.')
+        except discord.ExtensionAlreadyLoaded:
+            await ctx.respond(f'Cog {extension} is already loaded.')
+        except discord.ExtensionNotFound:
+            await ctx.respond(f'Cog {extension} was not found, please try again.')
+
+
+def setup(bot):
+    bot.add_cog(Developer(bot))
