aee50532413faee06c954f410eedc4c8b75c8d89
Austin Hargis <theaustinhargis@gmail.com>
2022-09-29 15:16:54 -0400

    Enhanced the 'about' slash command. Resolved an oversight that led to the 'about' slash command not working.

--- cogs/misc.py
+++ cogs/misc.py
@@ -8,16 +8,20 @@ class Misc(commands.Cog):
         self.bot = bot
 
     @commands.slash_command(name='about', description='Provides information about Shrimpy')
-    async def about(self):
+    async def about(self, ctx):
         embed = discord.Embed(
-            title='About Shrimpy',
+            title='Shrimpy',
             description='Version: 0.1.0a',
             color=discord.Color.blurple()
         )
 
+        embed.add_field(name='About Shrimpy', value='Shrimpy originally began development in March of 2021. The backend for Shrimpy has been through several revisions due to changing Discord APIs. However, pycord seems to be a stable and well-supported open-source API, so Shrimpy should be able to stick around from here on out.', inline=False)
         embed.add_field(name='Technology', value='Shrimpy is written in Python 3.10 with Pycord v2.1.1.')
-        embed.set_author(name='Austin Hargis')
-        embed.set_footer(text='https://github.com/theaustinhargis/ShrimpyRewrite')
+        embed.add_field(name='GitHub', value='[https://github.com/theaustinhargis/ShrimpyRewrite](https://github.com/theaustinhargis/ShrimpyRewrite)')
+        embed.set_author(name='Austin Hargis', icon_url='https://avatars.githubusercontent.com/u/25471876?v=4')
+        embed.set_footer(text='Created by Austin Hargis & Contributors - 09/29/22')
+
+        await ctx.respond(embed=embed)
 
 
 def setup(bot):
