fa9367bac9499ee99402c28859c9be684539df8b
Austin Hargis <theaustinhargis@gmail.com>
2022-09-29 15:24:57 -0400

    Fixed some language issues

--- cogs/developer.py
+++ cogs/developer.py
@@ -13,7 +13,7 @@ class Developer(commands.Cog):
     async def latency(self, ctx):
         await ctx.respond(f'Current latency is {self.bot.latency:.2f}ms')
 
-    @commands.slash_command(name="reload_extension", description="Reloads specified extensions(s)")
+    @commands.slash_command(name="reload_extension", description="Reloads specified extension")
     async def reload_cog(self, ctx, extension: discord.Option(discord.SlashCommandOptionType.string, description="An extension for Shrimpy")):
         if ctx.author.id == int(os.getenv('OWNER')):
             self.bot.reload_extension(f'cogs.{extension}')
@@ -21,7 +21,7 @@ class Developer(commands.Cog):
         else:
             await ctx.respond(f'You don\'t have permission to use this command.')
 
-    @commands.slash_command(name="load_ext", description="Loads specified extensions(s)")
+    @commands.slash_command(name="load_ext", description="Loads specified extension")
     async def load_cog(self, ctx, extension: discord.Option(discord.SlashCommandOptionType.string, description="An extension for Shrimpy")):
         try:
             if ctx.author.id == int(os.getenv('OWNER')):
@@ -34,7 +34,7 @@ class Developer(commands.Cog):
         except discord.ExtensionNotFound:
             await ctx.respond(f'Cog {extension} was not found, please try again.')
 
-    @commands.slash_command(name="unload_extension", description="Unloads specified extension(s)")
+    @commands.slash_command(name="unload_extension", description="Unloads specified extension")
     async def unload_cog(self, ctx, extension: discord.Option(discord.SlashCommandOptionType.string, description="An extension for Shrimpy")):
         try:
             if ctx.author.id == int(os.getenv('OWNER')):
