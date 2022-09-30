05835d86f8478d84da377f856d31d8b1b6b9dab5
Austin Hargis <theaustinhargis@gmail.com>
2022-09-30 11:08:26 -0400

    Implemented 'randomnum' slash command

--- cogs/games.py
+++ cogs/games.py
@@ -1,3 +1,5 @@
+import random
+
 import discord
 from discord.ext import commands
 
@@ -11,6 +13,10 @@ class Games(commands.Cog):
     async def ping(self, ctx):
         await ctx.respond(f'Pong!')
 
+    @commands.slash_command(name='randomnum', description='Get a random number generated for you')
+    async def randomnum(self, ctx, num1: discord.Option(discord.SlashCommandOptionType.integer), num2: discord.Option(discord.SlashCommandOptionType.integer)):
+        randnum = random.randint(num1, num2)
+        await ctx.respond(f'Your random number between {num1} and {num2} is {randnum}!')
 
 def setup(bot):
     bot.add_cog(Games(bot))
