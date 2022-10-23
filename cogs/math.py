0f162f259e53acf7d5e6669826250658827c3374
Austin Hargis <theaustinhargis@gmail.com>
2022-10-23 11:57:21 -0400

    Moved all math functions to Discord SlashCommandGroups

--- cogs/math.py
+++ cogs/math.py
@@ -4,30 +4,32 @@ from discord.ext import commands
 
 class Math(commands.Cog):
 
+    math = discord.SlashCommandGroup(name="math", description="Mathematic functions")
+
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name="add_values", description="Add values together")
+    @math.command(name="add", description="Add two values together")
     async def add_values(self, ctx, int1: int, int2: int):
         await ctx.respond(f"The sum of those values is {int1+int2}.")
 
-    @commands.slash_command(name="subtract_values", description="Subtract values")
+    @math.command(name="subtract", description="Subtract two values")
     async def subtract_values(self, ctx, int1: int, int2: int):
         await ctx.respond(f"The difference of those values is {int1-int2}.")
 
-    @commands.slash_command(name="multiply_values", description="Multiply values")
+    @math.command(name="multiply", description="Multiply two values")
     async def multiply_values(self, ctx, int1: int, int2: int):
         await ctx.respond(f"The product of those values is {int1*int2}.")
 
-    @commands.slash_command(name="divide_valies", description="Divide values")
+    @math.command(name="divide", description="Divide two values")
     async def divide_values(self, ctx, int1: int, int2: int):
         await ctx.respond(f"The quotient of those values is {int1/int2}")
 
-    @commands.slash_command(name="exponential", description="Solves an exponential equation")
+    @math.command(name="exponential", description="Solves an exponential equation")
     async def exponential(self, ctx, base: float, exponent: float):
         await ctx.respond(f"The solution to your exponential is {base**exponent}.")
 
-    @commands.slash_command(name="list_factors", description="List factors of an integer")
+    @math.command(name="factors", description="List factors of an integer")
     async def list_factors(self, ctx, integer: int):
         factors = []
         msg = f"```\nFactors of {integer}:\n"
