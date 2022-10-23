92ee30f8529ceea2d9eafe6683b5626e88fa7001
Austin Hargis <theaustinhargis@gmail.com>
2022-10-23 12:00:56 -0400

    Moved all extension functions to Discord SlashCommandGroups

--- cogs/developer.py
+++ cogs/developer.py
@@ -6,6 +6,8 @@ from discord.ext import commands
 
 class Developer(commands.Cog):
 
+    extensions = discord.SlashCommandGroup(name="extension", description="Group of extension related commands")
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -13,7 +15,7 @@ class Developer(commands.Cog):
     async def latency(self, ctx):
         await ctx.respond(f'Current latency is {self.bot.latency:.2f}ms')
 
-    @commands.slash_command(name="reload_extension", description="Reloads specified extension")
+    @extensions.command(name="reload", description="Reloads specified extension")
     async def reload_cog(self, ctx, extension: discord.Option(discord.SlashCommandOptionType.string, description="An extension for Shrimpy")):
         if ctx.author.id == int(os.getenv('OWNER')):
             self.bot.reload_extension(f'cogs.{extension}')
@@ -21,7 +23,7 @@ class Developer(commands.Cog):
         else:
             await ctx.respond(f'You don\'t have permission to use this command.')
 
-    @commands.slash_command(name="load_extension", description="Loads specified extension")
+    @extensions.command(name="load", description="Loads specified extension")
     async def load_cog(self, ctx, extension: discord.Option(discord.SlashCommandOptionType.string, description="An extension for Shrimpy")):
         try:
             if ctx.author.id == int(os.getenv('OWNER')):
@@ -34,7 +36,7 @@ class Developer(commands.Cog):
         except discord.ExtensionNotFound:
             await ctx.respond(f'Cog {extension} was not found, please try again.')
 
-    @commands.slash_command(name="unload_extension", description="Unloads specified extension")
+    @extensions.command(name="unload", description="Unloads specified extension")
     async def unload_cog(self, ctx, extension: discord.Option(discord.SlashCommandOptionType.string, description="An extension for Shrimpy")):
         try:
             if ctx.author.id == int(os.getenv('OWNER')):
