589adfdb9faae1f6315338091ae1639896dac4b3
Austin Hargis <theaustinhargis@gmail.com>
2022-12-08 22:55:37 -0500

    Implemented paycheck command. Updated registration command.

--- cogs/economy.py
+++ cogs/economy.py
@@ -1,17 +1,14 @@
+from datetime import datetime
 import discord
-from discord.ext import commands
 import sqlite3
 
 db = sqlite3.Connection('./economy.db')
 tables = db.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
 if len(tables) == 0:
-    db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, balance INTEGER)")
-else:
-    pass
+    db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, balance INTEGER, last_paycheck INTEGER)")
 
 
 class Economy(discord.Cog):
-
     economy = discord.SlashCommandGroup(name="economy", description="A group of Shrimpy Economy commands")
 
     def __init__(self, bot):
@@ -27,21 +24,39 @@ class Economy(discord.Cog):
     async def transfer_balance(self, context, user: discord.Member, amount: discord.SlashCommandOptionType.integer):
         pass
 
-    # TODO: implement paycheck command
     @economy.command(name="paycheck", description="Collect your weekly paycheck, if one is available")
     async def paycheck(self, context):
-        pass
+        row = db.execute(f"SELECT * FROM users WHERE id=?", (context.author.id,)).fetchone()
+        if row is not None:
+            last_paycheck_date = datetime.strptime(str(row[2]), '%Y%m%d%H%M%S')
+            last_paycheck_diff = (datetime.today() - last_paycheck_date).days
+
+            if last_paycheck_diff >= 7:
+                today = datetime.today().strftime("%Y%m%d%H%M%S")
+                db.execute(f'UPDATE users SET last_paycheck = {today}, balance = {row[1] + 500} WHERE id = '
+                           f'{context.author.id}')
+                db.commit()
+                await context.respond(f"Hello <@{context.author.id}>! Your paycheck of $500 has been dispensed.")
+            else:
+                await context.respond(f"Hello <@{context.author.id}>! You still have {7 - last_paycheck_diff} days "
+                                      f"until your next paycheck.")
+        else:
+            await context.respond(f"Hello <@{context.author.id}>! In order to collect your paycheck, you must first "
+                                  f"run '/economy register'.")
+            # await context.respond(f"User {row[0]}, you have ${row[1]}, and you got your last paycheck on {row[2]}")
 
     @economy.command(name="register", description="Register for the Shrimpy Economy")
     async def register(self, context):
         row = db.execute(f"SELECT * FROM users WHERE id=?", (context.author.id,)).fetchone()
-
+        today = datetime.today().strftime("%Y%m%d%H%M%S")
         if row is None:
-            db.execute(f'INSERT INTO users (id, balance) VALUES ({context.author.id}, {500})')
+            db.execute(f'INSERT INTO users (id, balance, last_paycheck) VALUES ({context.author.id}, {500}, {today})')
+            db.commit()
             await context.respond(f"Congrats, you have been registered with the Shrimpy Economy. Your starting "
                                   f"balance is $500.")
         else:
             await context.respond(f"You have already registered with the Shrimpy Economy.")
+
 
 def setup(bot):
     bot.add_cog(Economy(bot))
