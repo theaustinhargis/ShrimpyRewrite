4b04fd0af18450d580dc85db8e194f0465f71da4
Austin Hargis <theaustinhargis@gmail.com>
2022-12-23 19:20:31 -0500

    Updated some information in the 'about' command

--- cogs/misc.py
+++ cogs/misc.py
@@ -16,7 +16,7 @@ class Misc(commands.Cog):
         )
 
         embed.add_field(name="About Shrimpy", value="Shrimpy originally began development in March of 2021. The backend for Shrimpy has been through several revisions due to changing Discord APIs. However, pycord seems to be a stable and well-supported open-source API, so Shrimpy should be able to stick around from here on out.", inline=False)
-        embed.add_field(name="Technology", value="Shrimpy is written in Python 3.11 with Pycord v2.3.0.")
+        embed.add_field(name="Technology", value="Shrimpy is written in Python 3.11.1 with Pycord v2.3.2.")
         embed.add_field(name="GitHub", value='[https://github.com/theaustinhargis/ShrimpyRewrite](https://github.com/theaustinhargis/ShrimpyRewrite)')
         embed.set_author(name="Austin Hargis", icon_url='https://avatars.githubusercontent.com/u/25471876?v=4')
         embed.set_footer(text="Created by Austin Hargis & Contributors - 09/29/22")
